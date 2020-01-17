7173f511cc57ee9ed72804c6c41a419b258d4f41
HandidaT <hTromep@gmail.com>
2020-01-17 21:16:29 +0530

    Changed muxing.c stream duration and FPS

--- video/muxing.c
+++ video/muxing.c
@@ -10,7 +10,7 @@
 #include <libavformat/avformat.h>
 #include <libswscale/swscale.h>
 #include <libswresample/swresample.h>
-#define STREAM_DURATION   10.0
+#define STREAM_DURATION   1.0
 #define STREAM_FRAME_RATE 25 /* 25 images/s */
 #define STREAM_PIX_FMT    AV_PIX_FMT_YUV420P /* default pix_fmt */
 #define SCALE_FLAGS SWS_BICUBIC
